309c57f29cf17e53a998ef0e246c3fc31114ce45
Khoi <khoii87@gmail.com>
2020-04-03 21:05:48 +0200

    feat: :art: Added the Grid and CSS Files that for the grid
    :fire: CSS files that isn't used

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
-import './assets/css/core.css';
+import React from "react";
+import "./assets/css/core.css";
 
 function App() {
   return (
-    <div className="App">
-        
+    <div class="grid-container">
+      <div class="total-sign-ups">Total Sign ups</div>
+      <div class="graph-individuals">Graph Individual</div>
+      <div class="dropdown-window"> Dropdown Window</div>
+      <div class="graph-companies">Graph Companies</div>
+      <div class="latest-signups">latest-signups</div>
+      <div class="navbar">Navbar</div>
+      <div class="analytics-tops">Analytics Tops</div>
+      <div class="analytics-bottom">Analytics Bottom</div>
+      <div class="graph-menu">Graph Menu</div>
+      <div class="aside">Aside</div>
     </div>
+
+    /* <div className="grid-container">
+      <aside></aside>
+      <header>
+        <nav></nav>
+        <div class="analytics-tops"></div>
+        <div class="analytics-bottom"></div>
+        <menu>
+          <menuitem class="graph-menu"></menuitem>
+        </menu>
+      </header>
+      <main>
+      <div class="graph-individuals"></div>
+      <div class="graph-companies"></div>
+      <div class="total-sign-ups"></div>
+      <div class="dropdown-window"></div>
+      <div class="latest-signups"></div>
+
+      </main>
+    </div> */
   );
 }
 
