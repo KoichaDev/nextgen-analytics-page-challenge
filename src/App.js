359b1f6ad0c32e24a333346d4184ac29513b774f
Khoi <khoii87@gmail.com>
2020-04-03 21:22:05 +0200

    feat: :art: Added CSS Colors for the navbar, analytics header, aside and  body

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./assets/css/core.css";
 
 function App() {
   return (
-    <div class="grid-container">
+     <div class="grid-container">
       <div class="total-sign-ups">Total Sign ups</div>
       <div class="graph-individuals">Graph Individual</div>
       <div class="dropdown-window"> Dropdown Window</div>
@@ -14,24 +14,24 @@ function App() {
       <div class="analytics-bottom">Analytics Bottom</div>
       <div class="graph-menu">Graph Menu</div>
       <div class="aside">Aside</div>
-    </div>
+    </div> 
 
     /* <div className="grid-container">
-      <aside></aside>
+      <aside>Aside</aside>
       <header>
-        <nav></nav>
-        <div class="analytics-tops"></div>
-        <div class="analytics-bottom"></div>
+        <nav>Navbar</nav>
+        <div class="analytics-tops">Analtyics tops</div>
+        <div class="analytics-bottom">Analytics Bottom</div>
         <menu>
-          <menuitem class="graph-menu"></menuitem>
+          <div class="graph-menu">Graph Menu</div>
         </menu>
       </header>
       <main>
-      <div class="graph-individuals"></div>
-      <div class="graph-companies"></div>
-      <div class="total-sign-ups"></div>
-      <div class="dropdown-window"></div>
-      <div class="latest-signups"></div>
+      <div class="graph-individuals">Graph Individuals</div>
+      <div class="graph-companies">Graph Companies</div>
+      <div class="total-sign-ups">Total Sign Ups</div>
+      <div class="dropdown-window">Dropdown Window</div>
+      <div class="latest-signups">Latest Signups</div>
 
       </main>
     </div> */
