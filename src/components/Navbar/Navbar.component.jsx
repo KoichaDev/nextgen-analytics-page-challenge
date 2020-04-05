9988d8074e90933fc92a7065d1789b628859036a
Khoi <khoii87@gmail.com>
2020-04-05 18:06:28 +0200

    fix: :bug: :art:  Fixed a partially bug from nav menu, and added gradient color on the right side of the navbar

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -5,40 +5,46 @@ import Avatar from "./../../assets/images/avatar.png";
 const NavBar = () => {
   return (
     <>
-      <nav>
-        <ul className="nav-ul">
-          <li>
-            <a href="#">
-              <img
-                className="logo"
-                alt="Innovasjon Norge"
-                src={InnovasjonLogo}
-              />
-            </a>
-          </li>
-          <li>
-            <p>Account</p>
-          </li>
-          <li>
-            <p>Charts</p>
-          </li>
-          <li>
-            <p>Help</p>
-          </li>
-          <li>
-            <img src={Avatar} alt="Avatar" />
-          </li>
-          <li>
-            <p>Elise Andersen</p>
-          </li>
-          <li>
-            <p>
-              <small>17-02.2020</small>
-              <br />
-              2.45 PM
-            </p>
-          </li>
-        </ul>
+      <nav className="navbar-grid-container">
+        <div class="grid-navbar-container__navbar__left">
+          <ul>
+            <li>
+              <a href="#">
+                <img
+                  className="logo"
+                  alt="Innovasjon Norge"
+                  src={InnovasjonLogo}
+                />
+              </a>
+            </li>
+            <li>
+              <p>ACCOUNT</p>
+            </li>
+            <li>
+              <p>CHARTS</p>
+            </li>
+            <li>
+              <p>HELP</p>
+            </li>
+          </ul>
+        </div>
+        <div class="grid-navbar-container__navbar__right">
+          <ul>
+            <li>
+              <img src={Avatar} alt="Avatar" />
+            </li>
+            <li>
+              <p>Elsa Andersen</p>
+            </li>
+            <li>
+              <p>
+                <small>17-02.2020</small>
+                <br />
+                2.45 PM
+              </p>
+            </li>
+          </ul>
+        </div>
       </nav>
     </>
   );
